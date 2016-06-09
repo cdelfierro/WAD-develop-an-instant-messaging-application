bd7f68831c43d2b525dd1c11ee38966038327844
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2016-06-09 00:51:49 -0400

    Implement data reading security

--- server/main.js
+++ server/main.js
@@ -16,3 +16,16 @@ Meteor.startup(function () {
         }
     }
 });
+
+Meteor.publish("users", function() {
+    return Meteor.users.find();
+});
+
+Meteor.publish("chats", function() {
+    return Chats.find({
+        $or: [
+            {user1Id: this.userId},
+            {user2Id: this.userId}
+        ]
+    });
+});
